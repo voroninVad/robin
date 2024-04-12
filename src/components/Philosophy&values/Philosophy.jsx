7cdae351bb1e54c9c31d1d67c2b33fe5acd205ea
Vadim Voronin <vadimvoronin831@gmail.com>
2024-04-12 12:23:42 +0300

    add block Philosophy & values

---
+++ src/components/Philosophy&values/Philosophy.jsx
@@ -0,0 +1,13 @@
+import './philosophy.css'
+
+import React, { Component } from 'react'
+
+export class Philosophy extends Component {
+  render() {
+    return (
+      <div>Philosophy</div>
+    )
+  }
+}
+
+export default Philosophy
